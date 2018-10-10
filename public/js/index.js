fe2178d256f0b6c2a07d0bf0b687a8a4dd88b35f
Alexander Richardson <alexrich700@gmail.com>
2018-10-10 22:32:52 +0000

    Fixes

--- public/js/index.js
+++ public/js/index.js
@@ -8,3 +8,29 @@ function toggleAccordion(){
 }
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+///////////////////////////////// CONNECT TO API  //////////////////////////////////////////
+
+$(document).ready(function(){
+  $.getJSON("https://rest-api-alexrich700.c9users.io/api/text")
+  .then(app);
+  function app(text){
+    console.log(text.text);
+  }
+})
+
+//displays text
+function addTexts(texts) {
+  //add texts to page here
+  texts.forEach(function(text){
+    addText(text);
+  });
+}
+
+//format text
+function addText(text){
+  var newText = $('<div class="sermon-text col-md-6">'+text.text +'</div><div class="sermon-text sermon-speaker col-md-6">' +text.name+'</div>');
+  newText.data('id', text._id);
+  $('.list').append(newText);
+}
