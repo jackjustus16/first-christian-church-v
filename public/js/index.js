afff81aefc3c816ed720bc9bea205aea077aa2fa
Alexander Richardson <alexrich700@gmail.com>
2018-12-28 08:37:09 +0000

    Mobile nav fixes, prayer background photo add

--- public/js/index.js
+++ public/js/index.js
@@ -12,25 +12,25 @@ items.forEach(item => item.addEventListener('click', toggleAccordion));
 
 ///////////////////////////////// CONNECT TO API  //////////////////////////////////////////
 
-$(document).ready(function(){
-  $.getJSON("https://rest-api-alexrich700.c9users.io/api/text")
-  .then(app);
-  function app(text){
-    console.log(text[0].text);
-  }
-})
+// $(document).ready(function(){
+//   $.getJSON("https://rest-api-alexrich700.c9users.io/api/text")
+//   .then(app);
+//   function app(text){
+//     console.log(text[0].text);
+//   }
+// })
 
-//displays text
-function addTexts(texts) {
-  //add texts to page here
-  texts.forEach(function(text){
-    addText(text);
-  });
-}
+// //displays text
+// function addTexts(texts) {
+//   //add texts to page here
+//   texts.forEach(function(text){
+//     addText(text);
+//   });
+// }
 
-//format text
-function addText(text){
-  var newText = $('<div class="sermon-text col-md-6">'+text.text +'</div><div class="sermon-text sermon-speaker col-md-6">' +text.name+'</div>');
-  newText.data('id', text._id);
-  $('.list').append(newText);
-}
+// //format text
+// function addText(text){
+//   var newText = $('<div class="sermon-text col-md-6">'+text.text +'</div><div class="sermon-text sermon-speaker col-md-6">' +text.name+'</div>');
+//   newText.data('id', text._id);
+//   $('.list').append(newText);
+// }
